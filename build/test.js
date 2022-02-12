c700d14bea4a953d79b24fa6bcb2b7da41957254
lijiaqi.defined <lijq1103@163.com>
2022-02-13 00:43:14 +0800

    feat: update & delete & diff

--- build/test.js
+++ build/test.js
@@ -4,5 +4,104 @@
 var element = LReact.createElement(
   "div",
   null,
-  "hi"
+  LReact.createElement(
+    "h1",
+    null,
+    "Hi. I'm lijiaqi."
+  ),
+  LReact.createElement(
+    "h2",
+    null,
+    "My GitHub address is ",
+    LReact.createElement(
+      "a",
+      { href: "https://github.com/gaearon-byte" },
+      "gaearon-byte"
+    )
+  ),
+  LReact.createElement(
+    "div",
+    null,
+    "hi, this is div element."
+  ),
+  LReact.createElement(
+    "p",
+    null,
+    "hi, this is p element"
+  ),
+  LReact.createElement(
+    "span",
+    null,
+    "hi, this is span element."
+  ),
+  LReact.createElement("input", { type: "text" }),
+  LReact.createElement(
+    "select",
+    { name: "", id: "" },
+    LReact.createElement(
+      "option",
+      { value: "\u54C8\u54C8" },
+      "\u54C8\u54C8"
+    )
+  ),
+  LReact.createElement(
+    "ul",
+    null,
+    LReact.createElement(
+      "li",
+      null,
+      "this is first li"
+    ),
+    LReact.createElement(
+      "li",
+      null,
+      "this is second li"
+    )
+  ),
+  LReact.createElement(
+    "ul",
+    null,
+    LReact.createElement(
+      "ol",
+      null,
+      "this is first ol"
+    ),
+    LReact.createElement(
+      "ol",
+      null,
+      "this is second ol"
+    )
+  ),
+  LReact.createElement(
+    "table",
+    null,
+    LReact.createElement(
+      "tr",
+      null,
+      LReact.createElement(
+        "td",
+        null,
+        "0-0"
+      ),
+      LReact.createElement(
+        "td",
+        null,
+        "0-1"
+      )
+    ),
+    LReact.createElement(
+      "tr",
+      null,
+      LReact.createElement(
+        "td",
+        null,
+        "1-0"
+      ),
+      LReact.createElement(
+        "td",
+        null,
+        "1-1"
+      )
+    )
+  )
 );
--- src/test.jsx
+++ src/test.jsx
@@ -1,2 +1,30 @@
 /** @jsx LReact.createElement */
-const element = <div>hi</div>
+const element = <div>
+  <h1>Hi. I'm lijiaqi.</h1>
+  <h2>My GitHub address is <a href="https://github.com/gaearon-byte">gaearon-byte</a></h2>
+  <div>hi, this is div element.</div>
+  <p>hi, this is p element</p>
+  <span>hi, this is span element.</span>
+  <input type="text" />
+  <select name="" id="">
+    <option value="哈哈">哈哈</option>
+  </select>
+  <ul>
+    <li>this is first li</li>
+    <li>this is second li</li>
+  </ul>
+  <ul>
+    <ol>this is first ol</ol>
+    <ol>this is second ol</ol>
+  </ul>
+  <table>
+    <tr>
+      <td>0-0</td>
+      <td>0-1</td>
+    </tr>
+    <tr>
+      <td>1-0</td>
+      <td>1-1</td>
+    </tr>
+  </table>
+</div>
